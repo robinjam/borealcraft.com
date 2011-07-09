0ddc606230ae23bf3ab89f4e7055da00a9b56782
James Robinson <robinjam304@hotmail.com>
2011-07-09 02:10:08 +0100

    Add server status widget

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :server_online?
+
+  protected
+
+  def server_online?
+    return !`screen -ls | grep "\.minecraft"`.empty?
+  end
 end
