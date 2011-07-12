7a039faa5cfd0b5a828d5bb1b9b1e8219f848283
James Robinson <robinjam304@hotmail.com>
2011-07-12 14:08:10 +0100

    Add another screenshot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,28 +8,32 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(function() {
-  // Load all the screenshots
-  for (var i = 1; i <= 5; i++)
-    $(".wrapper > header").append('<img src="/assets/screenshots/' + i + '.png" data-id="' + i + '" class="screenshot"/>');
+(function($) {
+  var num_screenshots = 6;
+  
+  $(function() {
+    // Load all the screenshots
+    for (var i = 1; i <= num_screenshots; i++)
+      $(".wrapper > header").append('<img src="/assets/screenshots/' + i + '.png" data-id="' + i + '" class="screenshot"/>');
 
-  cycleScreenshots(0);
+    cycleScreenshots(0);
 
-  // Hide the flash messages
-  $(".flash").delay(5000).slideUp(500);
-});
-
-// n = current image
-function cycleScreenshots(n) {
-  var nextImageId = (n + 1) % 5 + 1;
-  var nextImage = getImage(nextImageId);
-  nextImage.delay(5000).css("z-index", n);
-  nextImage.fadeIn(1000, function() {
-    getImage(n % 5 + 1).hide();
-    cycleScreenshots(n + 1);
+    // Hide the flash messages
+    $(".flash").delay(5000).slideUp(500);
   });
-}
 
-function getImage(n) {
-  return $(".wrapper > header img[data-id='" + n + "']");
-}
+  // n = current image
+  function cycleScreenshots(n) {
+    var nextImageId = (n + 1) % num_screenshots + 1;
+    var nextImage = getImage(nextImageId);
+    nextImage.delay(5000).css("z-index", n);
+    nextImage.fadeIn(1000, function() {
+      getImage(n % num_screenshots + 1).hide();
+      cycleScreenshots(n + 1);
+    });
+  }
+
+  function getImage(n) {
+    return $(".wrapper > header img[data-id='" + n + "']");
+  }
+})(jQuery);
