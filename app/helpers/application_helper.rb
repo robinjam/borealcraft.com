616157997b322d44f74fff2301773f890020f609
James Robinson <robinjam304@hotmail.com>
2012-01-04 21:06:12 +0000

    Move published_date_of into ApplicationHelper and add options to markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
-  def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  def markdown(text, attrs = {})
+    renderer = Redcarpet::Render::HTML.new(attrs)
+    markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(text).html_safe
+  end
+
+  def published_date_of(post)
+    if post.created_at < 1.day.ago
+      post.created_at.strftime "#{post.created_at.day.ordinalize} %B %Y"
+    else
+      "#{time_ago_in_words post.created_at} ago"
+    end
   end
 end
--- app/helpers/headlines_helper.rb
+++ app/helpers/headlines_helper.rb
@@ -1,9 +1,3 @@
 module HeadlinesHelper
-  def published_date_of(post)
-    if post.created_at < 1.day.ago
-      post.created_at.strftime "#{post.created_at.day.ordinalize} %B %Y"
-    else
-      "#{time_ago_in_words post.created_at} ago"
-    end
-  end
+  
 end
