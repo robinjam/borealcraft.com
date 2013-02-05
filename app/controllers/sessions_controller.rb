94f3b7fca02d91edf0aa00acbdb0a91e24006677
James Robinson <admin@robinjam.net>
2013-02-05 02:37:27 +0000

    Add password reset hint to flash message if incorrect username or password is given

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_back notice: "Thank you for logging in, #{user.username}."
     else
-      redirect_back alert: 'Incorrect username or password.'
+      redirect_back alert: "Incorrect username or password.<br />Forgotten your password? Connect to the server and use the <strong>/get-token</strong> command to get a temporary replacement.".html_safe
     end
   end
 
