f2ad52b81330fe49fb01461b00f4597a385915ea
James Robinson <admin@robinjam.net>
2013-07-19 15:03:42 +0100

    Serve uploaded screenshots over https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ BorealCraft::Application.configure do
       bucket: ENV['S3_BUCKET'],
       access_key_id: ENV['S3_ACCESS_KEY_ID'],
       secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
-    }
+    },
+    s3_protocol: 'https'
   }
 end
