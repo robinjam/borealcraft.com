fe969639726ef42afe2947d4e5d449ecd20af500
James Robinson <admin@robinjam.net>
2013-07-19 14:31:08 +0100

    Update Paperclip, extract S3 bucket to env. var

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -5,13 +5,13 @@ class Screenshot < ActiveRecord::Base
   has_many :comments, as: :commentable, dependent: :destroy
 
   STORAGE_OPTIONS = Rails.env.production? ? {
+      path: ':attachment/:id/:style',
       storage: :s3,
-      bucket: 'mundus-meus',
       s3_credentials: {
+        bucket: ENV['S3_BUCKET'],
         access_key_id: ENV['S3_ACCESS_KEY_ID'],
         secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
-      },
-      path: ':attachment/:id/:style'
+      }
     } : {}
 
   has_attached_file :image, {
